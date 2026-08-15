dbbff693af3b87180926e3dbcdb8afc76f7393a8
Rana Gaber <158029182+ranagaber@users.noreply.github.com>
2026-08-15 18:34:04 +0300

    Implement save function for model and tokenizer
    Added a save function to save the model and tokenizer.

--- src/token_exp/main.py
+++ src/token_exp/main.py
@@ -6,10 +6,14 @@ def main(txt_path : str, model_id: str , type : str):
     tokenizer , new_tokens = expand_tokenizer(model_id , vocab) 
     model = adjust_model(model_id, type, tokenizer, new_tokens)
     return model, tokenizer
-
+def save(model , tokenizer , model_dir="new_model", tokenizer_dir="expanded_tokenizer"):
+    tokenizer.save_pretrained(tokenizer_dir)
+    model.save_pretrained(model_dir)
+    
 if __name__ == "__main__":
-    main(
+    model, tokenizer = main(
         txt_path="your/path",
         model_id="your/model",
         type="LM"
     )
+    save(model , tokenizer)
